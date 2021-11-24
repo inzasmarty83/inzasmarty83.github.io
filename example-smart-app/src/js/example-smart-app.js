360897d96328359f32eef3f2d36168b06cd57269
InzamamSiddiqui <inzasmarty83@gmail.com>
2021-11-24 15:14:46 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,21 +11,21 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+        // var obv = smart.patient.api.fetchAll({
+                    // type: 'Observation',
+                    // query: {
+                      // code: {
+                        // $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                              // 'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                              // 'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      // }
+                    // }
+                  // });
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
+        $.when(pt).done(function(patient) {
+          //var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
           var fname = '';
@@ -36,11 +36,11 @@
             lname = patient.name[0];
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          // var height = byCodes('8302-2');
+          // var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          // var hdl = byCodes('2085-9');
+          // var ldl = byCodes('2089-1');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
